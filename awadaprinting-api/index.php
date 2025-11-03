6091f2dc51864f3332ecb7f2d56a59dfd25f2da5
Aliibrahim4p <167108686+Aliibrahim4p@users.noreply.github.com>
2025-11-03 22:04:49 +0200

    predis installation

--- awadaprinting-api/index.php
+++ awadaprinting-api/index.php
@@ -1,16 +1,21 @@
 <?php
 require_once "config/db.php";
 
-$db = new Database();
-$conn = $db->connect();
 
-if ($conn) {
-    echo "✅ PostgreSQL connected successfully!";
-} else {
-    echo "❌ Connection failed.";
-}
+require_once "predis/autoload.php";
+use Predis\Client as PredisClient;
+
+$redis = new PredisClient();
+$redis->set("namw", "Ali");
+echo $redis->get("name");
+$r = new PredisClient([
+    'scheme' => 'tcp',
+    'host' => '127.0.0.1',
+    'port' => 6379,
+    'password' => '',
+    'database' => 0,
+]);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
